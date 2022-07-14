9d5de0e33f4c52401e8585c23286af73dc47f8f8
Dionovan Ramadhani <80899280+nyanboy1127@users.noreply.github.com>
2022-07-14 20:47:31 +0700

    fix: shoote mechanics

--- The House Of The Dead - Remake by Me/Assets/Scripts/Player Shoot/Shoot.cs
+++ The House Of The Dead - Remake by Me/Assets/Scripts/Player Shoot/Shoot.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    public GameObject bullet;
+    [SerializeField] LayerMask enemyMask;
 
     void Update()
     {
         PlayerShoot();
-
-       
     }
 
     void PlayerShoot()
     {
-        if (Input.GetMouseButtonDown(0))
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     Vector3 mousePosition = Input.mousePosition;
+        //     mousePosition.z = 1;
+
+        //     Vector3 screenWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        //     Instantiate(bullet, screenWorldPosition, transform.rotation);
+        // }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool isHit = Physics.Raycast(ray, out hit, 100, enemyMask);
+
+        if (isHit && Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition.z = 1;
-
-            Vector3 screenWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-            Instantiate(bullet, screenWorldPosition, transform.rotation);
+            // take damage
+            Destroy(hit.transform.gameObject);        
         }
     }
 }
